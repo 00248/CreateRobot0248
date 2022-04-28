6ce2febe7ed03a35533374ac1bffc766de31b62a
0248 <102600924+00248@users.noreply.github.com>
2022-04-28 09:30:52 -0400

    Add files via upload
    Added code for the arm to extend and grab botguy. Then the arm retracts, the create turns, goes to Airlock, and opens the claw.

--- create_code.c
+++ create_code.c
@@ -3,10 +3,11 @@
 int main()
 {
     shut_down_in(120);
+    enable_servos();
     create_connect();
-    create_drive_direct(250,260);
+    create_drive_direct(250,265);
     msleep(2700);
-     create_drive_direct(250,280);
+     create_drive_direct(250,290);
     msleep(1600);
     create_stop();
     mav(0,-1500);
@@ -20,7 +21,24 @@ int main()
     msleep(500);
     create_stop();
     mav(1,-1500);
-    msleep(5000);
+    msleep(6000);
+    ao();
+    set_servo_position(0,170);
+    mav(1,1500);
+    msleep(200);
+    ao();
+    set_servo_position(0,790);
+    create_drive_direct(250,-250);
+    msleep(1500);
+    mav(1,1500);
+    msleep(6000);
+    ao();
+    create_drive_direct(250,250);
+    msleep(1300);
+    mav(1,1500);
+    msleep(1000);
+    ao();
+    set_servo_position(0, 170);
     create_disconnect();
     return 0;
 }

efa6ff2a9aad9e7b0fbe3f57711a74b06f5a6358
0248 <102600924+00248@users.noreply.github.com>
2022-05-06 09:44:39 -0400

    Add files via upload
    The created code is adjusted. It can open the PCR consistently.

--- create_code.c
+++ create_code.c
@@ -5,35 +5,39 @@ int main()
     shut_down_in(120);
     enable_servos();
     create_connect();
-    create_drive_direct(260,250); //drive to pcr
-    msleep(3900);
-     create_drive_direct(250,260);
-    msleep(700);
+    create_drive_direct(250,255); //drive to pcr
+    msleep(2800);
+     create_drive_direct(250,270);
+    msleep(1800);
     create_stop(); // stop at pcr
     mav(0,-1500); // open pcr
-	msleep(1300);
-    ao();
-    create_drive_direct(-250,250);//turn and back up create
-        msleep(600);
-    create_drive_direct(-250,-250);
-    msleep(1200);
-    create_drive_direct(250,-250);
-    msleep(600);
-    create_stop();
-    mav(1,-1500);// extend arm #1
-    msleep(6000);
+	msleep(2000);
     ao();
     set_servo_position(0,170); // open claw
-    mav(1,1500); //extend arm #2
+    create_drive_direct(-150,-150);
+    msleep(200);
+    create_drive_direct(-200,200);//turn and back up create
+        msleep(700);
+    create_drive_direct(-200,-200);
+    msleep(1300);
+    create_drive_direct(200,-200);
+    msleep(700);
+    create_stop();
+    mav(1,1500);// extend arm #1
+    msleep(5000);
+    ao();
+     mav(1,1500); //extend arm #2
     msleep(200);
     ao();
     set_servo_position(0,790); // close claw
-    create_drive_direct(250,-250);// turn
+    create_drive_direct(50,-50);// turn
     msleep(500);
     create_stop();
-    mav(1,1500); // retract arm
-    msleep(6000);
+    mav(1,-1500); // retract arm
+    msleep(5000);
     ao();
+    create_drive_direct(250,-250);
+    msleep(500);
     create_drive_direct(250,250); //drive to Airlock
     msleep(3400);
     create_drive_direct(250,-250); //turn

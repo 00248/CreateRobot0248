abc0bc31fb161773b43c219bdeab693f1efea8a0
0248 <102600924+00248@users.noreply.github.com>
2022-05-10 09:40:00 -0400

    Add files via upload

--- create_code.c
+++ create_code.c
@@ -5,48 +5,33 @@ int main()
     shut_down_in(120);
     enable_servos();
     create_connect();
-    create_drive_direct(250,255); //drive to pcr
-    msleep(2800);
+    create_drive_direct(260,250); //drive to pcr
+    msleep(4500);
      create_drive_direct(250,270);
-    msleep(1800);
+    msleep(700);
     create_stop(); // stop at pcr
     mav(0,-1500); // open pcr
 	msleep(2000);
     ao();
     set_servo_position(0,170); // open claw
     create_drive_direct(-150,-150);
-    msleep(200);
+    msleep(1000);
     create_drive_direct(-200,200);//turn and back up create
         msleep(700);
     create_drive_direct(-200,-200);
-    msleep(1300);
+    msleep(2300);
     create_drive_direct(200,-200);
     msleep(700);
     create_stop();
     mav(1,1500);// extend arm #1
-    msleep(5000);
-    ao();
-     mav(1,1500); //extend arm #2
-    msleep(200);
+    msleep(5200);
     ao();
     set_servo_position(0,790); // close claw
-    create_drive_direct(50,-50);// turn
+    create_drive_direct(150,-150);// turn
     msleep(500);
     create_stop();
-    mav(1,-1500); // retract arm
-    msleep(5000);
-    ao();
     create_drive_direct(250,-250);
-    msleep(500);
-    create_drive_direct(250,250); //drive to Airlock
-    msleep(3400);
-    create_drive_direct(250,-250); //turn
-    msleep(500);
-    create_stop();
-    mav(1,1500); // extend arm #3
-    msleep(1000);
-    ao();
-    set_servo_position(0, 170);// open claw
+    
     create_disconnect();
     return 0;
 }
